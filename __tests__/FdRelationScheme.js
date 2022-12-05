97313f21db6715ac7d6b37b3c7fa36982f9bbf69
kensyo <takitateokowa@gmail.com>
2022-12-05 11:22:03 +0900

    style: Use single quote instead of double one

--- __tests__/FdRelationScheme.js
+++ __tests__/FdRelationScheme.js
@@ -5,25 +5,25 @@ const FdRelationScheme = FDRS.FdRelationScheme
 
 test('sample relation test', () => {
   const test_relation_scheme = new FdRelationScheme(
-    "test",
-    ["A", "B", "C"],
+    'test',
+    ['A', 'B', 'C'],
     [
-      { lhs: ["A", "B"], rhs: ["C"] },
-      { lhs: ["C"], rhs: ["A"] }
+      { lhs: ['A', 'B'], rhs: ['C'] },
+      { lhs: ['C'], rhs: ['A'] }
     ]
   );
 
-  expect(test_relation_scheme.fds[0]).toStrictEqual({ lhs: ["A", "B"], rhs: ["C"] });
+  expect(test_relation_scheme.fds[0]).toStrictEqual({ lhs: ['A', 'B'], rhs: ['C'] });
 });
 
 test('Fd range test', () => {
   function throwRangeError() {
     const test_relation_scheme = new FdRelationScheme(
-      "test",
-      ["A", "B", "C"],
+      'test',
+      ['A', 'B', 'C'],
       [
-        { lhs: ["A", "B"], rhs: ["C"] },
-        { lhs: ["C"], rhs: ["D"] }
+        { lhs: ['A', 'B'], rhs: ['C'] },
+        { lhs: ['C'], rhs: ['D'] }
       ]
     );
   }
--- __tests__/synthesis.js
+++ __tests__/synthesis.js
@@ -3,5 +3,5 @@
 const synthesis = require('../index')
 
 test('sample test', () => {
-  expect(synthesis.find_minimal_cover("hoge")).toBe("hoge")
+  expect(synthesis.find_minimal_cover('hoge')).toBe('hoge')
 });

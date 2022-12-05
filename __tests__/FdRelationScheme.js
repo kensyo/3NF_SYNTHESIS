96d61a39b3f2e0344831b100c14b3c5c8999cabb
kensyo <takitateokowa@gmail.com>
2022-12-05 11:02:57 +0900

    feat: Use an ordinary object instead of a class for fd

--- __tests__/FdRelationScheme.js
+++ __tests__/FdRelationScheme.js
@@ -2,16 +2,18 @@
 
 const FDRS = require('../lib/FdRelationScheme');
 const FdRelationScheme = FDRS.FdRelationScheme;
-const Fd = FDRS.Fd;
 
 test('sample relation test', () => {
     const test_relation_scheme = new FdRelationScheme(
         "test",
         ["A", "B", "C"],
-        [new Fd(["A", "B"], ["C"]), new Fd(["C"], ["A"])]
+        [
+            { lhs: ["A", "B"], rhs: ["C"] },
+            { lhs: ["C"], rhs: ["A"] }
+        ]
     );
 
-    expect(test_relation_scheme.fds[0]).toStrictEqual(new Fd(["A", "B"], ["C"]));
+    expect(test_relation_scheme.fds[0]).toStrictEqual({ lhs: ["A", "B"], rhs: ["C"] });
 });
 
 test('Fd range test', () => {
@@ -19,7 +21,10 @@ test('Fd range test', () => {
         const test_relation_scheme = new FdRelationScheme(
             "test",
             ["A", "B", "C"],
-            [new Fd(["A", "B"], ["C"]), new Fd(["C"], ["D"])]
+            [
+                { lhs: ["A", "B"], rhs: ["C"] },
+                { lhs: ["C"], rhs: ["D"] }
+            ]
         );
     }
 
